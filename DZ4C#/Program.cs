09dc52aed112be536127499f72b5a20107a2b806
sulta <sulta@DESKTOP-E785JP2>
2023-09-23 09:20:10 +0300

    Добавьте файлы проекта.

---
+++ DZ4C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZ4C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int start = int.Parse(Console.ReadLine());
+            int i = start;
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("число четное");
+            }
+            else
+            {
+                Console.WriteLine("не четное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
